9e1a2ad526340f44f8474a007a94ff2820d05449
Oleg Yamnikov <bgeyts667@gmail.com>
2020-10-21 15:03:37 +0500

    Translate the app into Russian

--- example-project/example/settings.py
+++ example-project/example/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -118,6 +118,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'onesocial_django', 'locale'),
+]
 
 
 # Static files (CSS, JavaScript, Images)
